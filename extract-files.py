b4adde926f61ea4dfef28a463db38a6c87092655
saku-bruh <notwolfyy@gmail.com>
2023-08-19 01:16:48 +0300

    edo: rebrand to sony convention
    Change-Id: Ib0c45ad6d5a1bba773f420829a2ecd9a2221128e
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -122,7 +122,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'sm8250-common',
+    'edo',
     'sony',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
